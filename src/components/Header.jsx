d6a67e501b9bdae5a43de957d9ba591a4796d460
Mohammad Shukuhi <mohammadhassanshukuhimd@gmail.com>
2024-09-03 02:48:57 +0330

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import LogoImg from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={LogoImg} alt="Restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
